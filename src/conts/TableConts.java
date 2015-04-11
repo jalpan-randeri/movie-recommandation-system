dd66510e09e817ac17f6142bf73ca221b4927790
Jalpan Randeri <jalpan@ccs.neu.edu>
2015-04-11 16:26:59 -0400

    added user-List<movie> table mapping to hbase

---
+++ src/conts/TableConts.java
@@ -0,0 +1,11 @@
+package conts;
+
+/**
+ * Created by jalpanranderi on 4/9/15.
+ */
+public class TableConts {
+    public static final String TABLE_USR_MOV_COLUMN_USR_ID = "USR_ID";
+    public static final String TABLE_USR_MOV_COLUMN_LIST_MOV = "LIST_MOV";
+    public static final String TABLE_NAME = "TABLE_USR_MOV_MAPPING";
+    public static final String TABLE_USR_MOV_COL_FAMILY = "movies";
+}
