e70d0a4d6f3225b2edec49e72fad6b69ab3a8b58
Jalpan Randeri <jalpan@ccs.neu.edu>
2015-04-12 19:34:48 -0400

    fixed null pointer hbase retrival

--- src/kmeans/utils/CentroidUtils.java
+++ src/kmeans/utils/CentroidUtils.java
@@ -1,10 +1,11 @@
 package kmeans.utils;
 
+import conts.KMeansConts;
 import conts.TableConts;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.KeyValue;
-import org.apache.hadoop.hbase.client.Get;
-import org.apache.hadoop.hbase.client.HTableInterface;
-import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.ZooKeeperConnectionException;
+import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.util.Bytes;
 
 import java.io.IOException;
@@ -16,24 +17,23 @@ import java.util.List;
  */
 public class CentroidUtils {
 
-    public static List<String> getCentroids(HTableInterface centroidTable,int k) throws IOException {
+    public static List<String> getCentroids(HTableInterface centroidTable, int k) throws IOException {
         List<String> list = null;
         for (int i = 0; i < k; i++) {
-            Get query = new Get(Bytes.toBytes(i));
-
+            Get query = new Get(String.valueOf(i).getBytes());
             query.setMaxVersions(1);
             Result row = centroidTable.get(query);
 
-            List<KeyValue> centroids = row.getColumn(Bytes.toBytes(TableConts.TABLE_CENTROID_FAMAILY),
+            byte[] cb = row.getValue(Bytes.toBytes(TableConts.TABLE_CENTROID_FAMAILY),
                     Bytes.toBytes(TableConts.TABLE_CENTROID_COLUMN_ID_CENTROID));
 
-            String centroid = Bytes.toString(centroids.get(0).getValue());
-
+            String centroid = Bytes.toString(cb);
             if (list == null) {
                 list = new ArrayList<>();
             }
-
             list.add(centroid);
+
+
         }
 
         return list;
@@ -42,24 +42,34 @@ public class CentroidUtils {
     public static List<String> getNewCentroids(HTableInterface newCentroidTable, int k) throws IOException {
         List<String> list = null;
         for (int i = 0; i < k; i++) {
-            Get query = new Get(Bytes.toBytes(i));
-
+            Get query = new Get(String.valueOf(i).getBytes());
             query.setMaxVersions(1);
             Result row = newCentroidTable.get(query);
 
-            List<KeyValue> centorids = row.getColumn(Bytes.toBytes(TableConts.TABLE_NEW_CENTROID_FAMAILY),
+            byte[] cb = row.getValue(Bytes.toBytes(TableConts.TABLE_NEW_CENTROID_FAMAILY),
                     Bytes.toBytes(TableConts.TABLE_NEW_CENTROID_COLUMN_ID_CENTROID));
 
-            String centroid = Bytes.toString(centorids.get(0).getValue());
-
+            String centroid = Bytes.toString(cb);
             if (list == null) {
                 list = new ArrayList<>();
             }
-
             list.add(centroid);
+
+
         }
+
 
         return list;
 
     }
+
+//    public static void main(String[] args) throws IOException {
+//        Configuration conf = new Configuration();
+//        HConnection con = HConnectionManager.createConnection(conf);
+//        HTableInterface table = con.getTable(TableConts.TABLE_NAME_CENTROID.getBytes());
+//
+//        List<String> list = getCentroids(table, KMeansConts.K);
+//
+//        System.out.println(list);
+//    }
 }
